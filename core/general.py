c94b0f8943253e4b128ffe43bd1130b9d1768fde
AkagiT <a.tatsuya@gmail.com>
2017-11-05 23:08:16 +0900

    update __new__ method in Attribute class

--- core/general.py
+++ core/general.py
@@ -6,7 +6,14 @@ except:
 
 
 class Attribute(unicode):
-    pass
+    _node = ''
+    _attr = ''
+
+    def __new__(cls, *args, **kwds):
+        if len(args) == 2:
+            cls._node = args[0]
+            cls._attr = args[1]
+        return super(Attribute, cls).__new__(cls, cls._node+"."+cls._attr)
 
 
 class Node(unicode):

6170daba9c911cff3b6ec71c8565b63f4f56031f
akiwoRM <a.tatsuya@gmail.com>
2018-03-27 23:45:04 +0900

    add reload function

--- __init__.py
+++ __init__.py
@@ -1,3 +1,11 @@
 # -*- coding:utf-8 -*-
+import sys
+
 __version__ = '0.0.1'
 __author__  = 'Tatsuya Akagi'
+
+
+def reload():
+    for mod in sys.modules.keys():
+        if mod.find("scwrap") > -1:
+            del sys.modules[mod]

ace13ffda54672550bf9859572cbef2fd69befe1
akiwoRM <a.tatsuya@gmail.com>
2018-10-15 00:41:49 +0900

    move files from core module to core_lib module

---
+++ core.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+
+from core_lib.general import *
+from core_lib.command import *
+from core_lib.node import *
--- core/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding:utf-8 -*-
-__version__ = '0.0.1'
-
-from general import *
-from command import *
-from node import *

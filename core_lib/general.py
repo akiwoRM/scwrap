706a35758a528e087749b9c81c73d53d0e528f1f
akiwoRM <a.tatsuya@gmail.com>
2019-11-11 19:05:40 +0900

    update repr result

--- core_lib/general.py
+++ core_lib/general.py
@@ -15,7 +15,7 @@ class Base(unicode):
     """Base class for Node class and Attribute class. This class has common methods like connection.
     """
     def __repr__(self):
-        return “{}(‘{}’)”.format(self.__class__.__name__, self)
+        return "{}('{}')".format(self.__class__.__name__, self)
 
     def connections(self, *args, **kwds):
         ret = cmds.listConnections(*args, **kwds)
